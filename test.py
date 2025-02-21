c69a92cd521525c497de75c4620895c9b1b6cf2e
T. Hamilton <profhamilton3@gmail.com>
2025-02-20 20:52:07 -0800

    lint cleanup test.py

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 # test.py
 import pandas as pd
 
-def test_dataset( ):
-    mydataset  = {
+def test_dataset():
+    mydataset = {
         'cars': ["BMW", "Volvo", "Ford", "Toyota", "Hyundai", "Daiwu"],
         'passings': [3, 7, 2, 9, 6, 2]
     }
     myvar = pd.DataFrame(mydataset)
     print("Pandas DataFrame")
-    assert( myvar.empty == False )
+    assert myvar.empty == False
     print(myvar) 
 
 def dataframe_test():

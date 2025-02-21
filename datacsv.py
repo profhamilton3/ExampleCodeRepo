8771d9c30f9cc21adb25f61c31afccf2fa7f14fa
T. Hamilton <profhamilton3@gmail.com>
2025-02-21 12:05:56 -0800

    Update datacsv.py

--- datacsv.py
+++ datacsv.py
@@ -4,23 +4,22 @@
 import pandas as pd
 
 class DataCsv:
-	def __init__(self):
-    	data = {
-			"calories": [420, 380, 390],
-			"duration": [50, 40, 45]
-		}
-		self.df = pd.DataFrame(data)
-		self.datacsv = none
-		print("INIT DATA FRAME:", df)
+    def _init__(self):
+        data = {
+            "calories": [420, 380, 390],
+            "duration": [50, 40, 45]
+        }
+        self.df = pd.DataFrame(data)
+        self.datacsv = none
+        print("INIT DATA FRAME:", df)
 
-	def get_df(self):
-    	return self.df
+    def get_df(self):
+        return self.df
 
-	def load_datacsv(self):
-    	if self.datacsv is none :
-        	self.datacsv = pandas.read_csv('./data.csv')
+    def load_datacsv(self):
+        if self.datacsv is none :
+            self.datacsv = pandas.read_csv('./data.csv')
         return self.datacsv
-    
+
     def get_datacsv(self):
         return self.loadcsv(self)
-    

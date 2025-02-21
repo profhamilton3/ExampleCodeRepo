94c158a3d2d855e12e6a825b900dd9b5218d6235
T. Hamilton <profhamilton3@gmail.com>
2025-02-21 11:57:19 -0800

    flake8 plint clean up

--- datacsv.py
+++ datacsv.py
@@ -4,21 +4,20 @@
 import pandas as pd
 
 class DataCsv:
-    def __init__(self):
+	def __init__(self):
     	data = {
-    		"calories": [420, 380, 390],
-  		"duration": [50, 40, 45]
-        	}
-	#load data into a DataFrame object:
-	self.df = pd.DataFrame(data)
-        self.datacsv = none
-	print("INIT DATA FRAME:", df)
-    
-    def get_df(self):
-        return self.df
-        
-    def load_datacsv(self):
-        if self.datacsv is none :
+			"calories": [420, 380, 390],
+			"duration": [50, 40, 45]
+		}
+		self.df = pd.DataFrame(data)
+		self.datacsv = none
+		print("INIT DATA FRAME:", df)
+
+	def get_df(self):
+    	return self.df
+
+	def load_datacsv(self):
+    	if self.datacsv is none :
         	self.datacsv = pandas.read_csv('./data.csv')
         return self.datacsv
     

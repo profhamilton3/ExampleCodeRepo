196966b22a622fde993b94e4730826caf8eb365a
T. Hamilton <profhamilton3@gmail.com>
2025-02-26 19:33:10 -0800

    flake test_runner.py

--- test_runner.py
+++ test_runner.py
@@ -5,6 +5,7 @@ import pandas
 import seaborn as sns
 import datacsv
 
+
 @pytest.fixture
 def df_data():
     ds = {
@@ -14,32 +15,38 @@ def df_data():
     dataframe = pandas.DataFrame(ds)
     return dataframe
 
+
 def test_one(df_data):
     print(df_data)
     assert (df_data).size > 0
+
 
 def test_two():
     s1 = pandas.Series([0, 1, 2, 4, 5, 6, 2])
     print(s1)
     assert s1.size > 0
-    
+
 
 def test_three(df_data):
     print((df_data).loc[0])
     assert (df_data).loc[0].size > 0
 
+
 def test_datacsv():
     dcsv = datacsv.DataCsv()
     assert 0 == dcsv.get_datacsv().size
+
 
 def test_datacsv_get_df():
     dcsv = datacsv.DataCsv()
     df = dcsv.get_df()
     assert df.size > 0
 
+
 def test_readcsv():
     d = pandas.read_csv('./data.csv')
     assert d.size > 0
+
 
 def test_datacsv_set_get_datacsv():
     data = pandas.read_csv('./data.csv')
@@ -48,6 +55,7 @@ def test_datacsv_set_get_datacsv():
     dcsv.set_datacsv(data)
     df = dcsv.get_datacsv()
     assert df.size > 0
+
 
 def test_edata_load():
     data = sns.load_data('flights')
